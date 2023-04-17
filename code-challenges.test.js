9ee077df5c98821ecc5320cf004c4cc523815dff
gnostertag <ostertag2000@gmail.com>
2023-04-17 08:19:50 -0700

    week5 assess

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -13,40 +15,163 @@
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
+describe('secretCode', () => {
+    const secretCodeWord1 = "Lackadaisical"
+    const secretCodeWord2 = "Gobbledygook"
+    const secretCodeWord3 = "Eccentric"
 
-const secretCodeWord1 = "Lackadaisical"
+it ('returns a string with the coded message', () => {
+
+    expect(secretCode(secretCodeWord1)).toEqual ("L4ck4d41s1c4l")
+    expect(secretCode(secretCodeWord2)).toEqual ("Gobbledygook")
+    expect(secretCode(secretCodeWord3)).toEqual ("Eccentric")
+})
+})
+
+// FAIL  ./code-challenges.test.js
+//   secretCode
+//     ✕ returns a string with the coded message (1 ms)
+//   singleLetters
+//     ✓ returns an array of all the words containing that specific letter (3 ms)
+//   fullHouse
+//     ✓ determines if the five numbers in the array are a full house and return a boolean (1 ms)
+
+//   ● secretCode › returns a string with the coded message
+
+//     ReferenceError: secretCode is not defined
+
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
+//Input: string
+//Output: string
+//Ill create a function called secret Code that takes sting as an argument. Next i think theres a way to use the replace method here, so our function should return the string but well have to use replace method for all instances of the secret code. So replace a for 4, e for 3, i for 1, and o for 0. I made that function and it is only returning the first occurance of each letter.Lackadaisical is coming back as L4ckada1sical. I need a way to account for all occurances of each letter. 
+//I used the replace all method to try and account for all occurrences of each letter. 
+//I researched g, the global operator which will account for all occurrences of a letter. 
+//When i run this function i keep getting error Command failed with exit code 1. I can't figure out what this means, I googled it and it seems there might be an issue with my yarn files. I think this code should work but there is just something wrong with my yarn it seems.
+
+
+const secretCode = (string) => {
+return string
+.replace(/a/g, '4')
+.replace(/A/g, '4')
+.replace(/E/g, '3')
+.replace(/e/g, '3')
+.replace(/i/g, '1')
+.replace(/I/g, '1')
+.replace(/O/g, '0')
+.replace(/o/g, '0')
+
+}
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe('singleLetters', () => {
 
-const letterA = "a"
+    const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+    const letterA = "a"
+    const letterE = "e"
+
+    it('returns an array of all the words containing that specific letter', () => {
+        expect(singleLetters(fruitArray, letterA)).toEqual (["Mango", "Apricot", "Peach"])
+        expect(singleLetters(fruitArray, letterE)).toEqual (["Cherry", "Blueberry", "Peach"])
+    })
+})
+
+// FAIL  ./code-challenges.test.js
+//   singleLetters
+//     ✕ returns an array of all the words containing that specific letter (1 ms)
+//   fullHouse
+//     ✓ determines if the five numbers in the array are a full house and return a boolean (2 ms)
+
+//   ● singleLetters › returns an array of all the words containing that specific letter
+
+//     ReferenceError: singleLetters is not defined
+
 // Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
+//Input: array
+//Output: array containing all the words containing all the words with that specific letter.
+// Create a function called singleLetters that takes in the array and the letter as arguments.
+// Then use filter to return a new array that contains only words with the given letter. Then we need a way to make sure the filter method ignores the casing of the letter and for that we could use tolowercase on the filtered value, and that value has to include the lower case letter. 
+//Return the filtered array.
+// const singleLetters = (arr, letter) => {
+//     const filteredArr = arr.filter((value) => {
+//         if (value.includes(letter)){
+//             return value
+//         } else if (value.includes(letter.toUpperCase)){
+//             return value
+//         } else {
+//             return 
+//         }
+//     })
+//     return filteredArr
+// }
+
+//*refactor*
+
+const singleLetters = (arr, letter) => {
+    return arr.filter((value) => 
+    value.toLowerCase().includes(letter.toLowerCase())
+    )}
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+describe ('fullHouse', () => {
+    const hand1 = [5, 5, 5, 3, 3]
+    const hand2 = [5, 5, 3, 3, 4]
+    const hand3 = [5, 5, 5, 5, 4]
+    const hand4 = [7, 2, 7, 2, 7]
+    it('determines if the five numbers in the array are a full house and return a boolean', () => {
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+        expect(fullHouse(hand4)).toEqual(true)
+    })
+})
+        
+// FAIL  ./code-challenges.test.js
+//   fullHouse
+//     ✕ determines if the five numbers in the array are a full house and return a boolean (1 ms)
+
+//   ● fullHouse › determines if the five numbers in the array are a full house and return a boolean
+
+//     ReferenceError: fullHouse is not defined
+
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+//Input: array
+//Output: true or false
+// I'll create a function called full house, Then I'll create an empty object called cards to store the card counts. Then create a for loop to loop through each element in the array, and check if the card count already exists in the object, if it does then increase the card count by 1, if not, then set the card count back to 1. I want to use object.values because I want the count stored in a temporary array. Lastly I'll use includes method to check for a count of 2 and a count of 3. The appropriate boolean value should be returned.
+
+// const fullHouse = (arr) => {
+//     const cards = {}
+//     for (let i=0; i<arr.length; i++){
+//             cards[arr[i]] = cards[arr[i]] + 1 || 1
+//         }
+//     const values = Object.values(cards)
+
+//     return values.includes(2) && Object.values(cards).includes(3)
+// }
+// *Refactor*
+ const fullHouse = (arr) => {
+        const cards = {}
+        for (let i of arr){
+                cards[i] = (cards[i] || 0) + 1
+            }
+        const values = Object.values(cards)
+    
+        return values.includes(2) && values.includes(3)
+    }
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: Super is used to add more information to a class in Ruby. When super is called it allows you to take code from another class and use it in a new one.
 
-Researched answer:
+Researched answer: Super is a method of communication between classes in Ruby. when super is called it  allows a subclass to invoke a method from a super class. Super is particularly useful because it allows us to reuse lines of code. 
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem is subset of the ruby language. Think of it this way, if ruby is a finished puzzle, the gems are all of the pieces that went in to making that puzzle complete.
 
-Researched answer:
+Researched answer: Gems are libraries of code functionality. Gems will gather your code together and make it easier to share, and implement into applcations. Gems can be used for many different things including web development, testing, and databases. Gems are popular in the ruby community because they make sharing code/components easier.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a database that stores data in rows and columns. Large collections of data are often put into relational databases so that the information is more organized and easily accessible. I have not worked with other types of databases but I will research that and get back to you!
 
-Researched answer:
+Researched answer: A relational database is a set of rows and columns, each one of these sets is called a table. This type of database is most similar to microsoft excel. There are actually many other types of databases, a few of them are spatial, document-oriented, and graph databases. These different types of databases all store different different types of data at different sizes and performance levels.
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: primary keys are used in the rails frame work, and they are used to identify each class. Using keys is an efficient way to id classes. They are important because they are usually simple and easy to identify.
 
-Researched answer:
+Researched answer: Primary keys are unique identifiers of each instance of a class. Using primary keys is a practice that ensures the consistency of data in tables and allows for swift retrieval of data, and that is why they are important. The most important use of a primary key is to establish a relationship between a table and a relational database. 
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: The verbs are all associated with actions of CRUD. I know crud stands for create read update and delete. Therefore it is safe to assume that the http verbs will all be somewhat related to creating, reading, updating, and deleting. 
 
-Researched answer:
+Researched answer: The HTTP verbs are get, post, put, patch, and delete. All of these requests correspond to a CRUD action. A get request allows us to display or read our content. Post is used when the client sends data or submits data to a server to Create a new resource. Put is used to Update the existing resource. Delete is used to Delete. Patch is also used to Update.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: Validations are filters that make sure that only the right clean and valid data will get saved into the database. Rails automatically runs validations before content is added to the database.
 
-2. RESTful routes:
+2. RESTful routes: Routes are the paths that allow apps like rails or react to respond to server requests. Routes usually map to the CRUD actions that are performed.
 
-3. ERB:
+3. ERB: ERB is an extension that stands for embedded ruby. ERB lets us write HTMLL code and drop ruby into the view file. 
 
-4. Params:
+4. Params: Params allow us to add more information to our programs. Info is added by passing values into the routes instead of directly to the url.
 
-5. API:
+5. API: Is the application programming interface, and it transmits data in the form of JSON. API is a set of rules and conventions that make it possible for different apps to communicate with eachother.
